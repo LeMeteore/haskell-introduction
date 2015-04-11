49ce4840af0c33972f3d703f9e9c3a5b5797171c
patrick nsukami <ndkpatt@gmail.com>
2015-04-11 04:05:26 +0000

    Do not really get it, hanoi exercice.

--- ex5.hs
+++ ex5.hs
@@ -6,7 +6,16 @@ x = [toUpper c | c <- s]
 -- defining a type synonym
 type Peg = String
 type Move = (Peg, Peg)
+type NumberOfPegs = Integer
+type NumberOfDiscs = Integer
+type NumberOfMoves = Integer
+
+-- moves = 2^discs -1 (only when number of pegs == 3)
+numberOfMoves::NumberOfDiscs -> NumberOfMoves
+numberOfMoves n
+  | n > 0 = 2^n -1
+  | otherwise = 0
 
 -- hanoi 2 "a" "b" "c" == [("a","c"), ("a","b"), ("c","b")]
-hanoi::Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi n "a" "b" "c" = []
+-- hanoi::Integer -> Peg -> Peg -> Peg -> [Move]
+-- hanoi n x y z = []
